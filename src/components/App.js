161d031e347106bf578ad72175b4e61d6cd1218f
tjbarkey <tjbarkey@gmail.com>
2019-11-10 22:40:07 -0800

    Updates
    Updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Router,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Content from "./Content";
@@ -9,8 +15,9 @@ import Blog from "./Blog";
 import BlogTagged from "./BlogTagged";
 import BlogPost from "./BlogPost";
 import WorldCup from "./WorldCup";
+import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
 import "../css/App.css";
-
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import {
@@ -21,9 +28,19 @@ import {
 
 library.add(fab, faFutbol, faBroadcastTower, faBars);
 
+const history = createBrowserHistory();
+
+// Initialize google analytics page view tracking
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+const trackingId = "UA-120634577-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter history={history}>
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
